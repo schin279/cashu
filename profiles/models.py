41cb2d085480a22f89de950a4dfdc9ffee2c94ef
Shaun <120163608+schin279@users.noreply.github.com>
2024-06-20 16:41:51 +1000

    Add files via upload

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Employee_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     dob = models.DateField()
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+
+class Employer_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
 
     def __str__(self):
